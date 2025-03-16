a9b59b9e2239c25c23d52b0c2bb4e813cd64b905
Farhanul Insan <gila.seal@yahoo.co.id>
2025-03-16 16:39:36 +0700

    Finish doing 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeap;
 
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) isLeap = true;
+            else isLeap = false;
+        } else isLeap = true;
+    } else isLeap = false;
+
+    return isLeap;
 };
 
 // Do not edit below this line
